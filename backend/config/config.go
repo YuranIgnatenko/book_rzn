97cedfb879d257b612662064d8c0c0abff60cb78
YuranIgnatenko <Yuran.ignatenko@yandex.ru>
2023-11-13 23:32:08 +0300

    исправлены ошибки ahref -> a href удалены лишнии конфиги и оставлен главный файл config.json изменен логотип сайта поправлены tab-кнопки разделы сайта

--- backend/config/config.go
+++ backend/config/config.go
@@ -3,33 +3,25 @@ package config
 import (
 	"backend/parsing"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"os"
 )
 
-var path_config = "/home/yu/Рабочий стол/code/book_rzn/backend/config/config.json"
-var path_datatemp = "/home/yu/Рабочий стол/code/book_rzn/backend/config/datatemp.json"
-var path_datatempCMS = "/home/yu/Рабочий стол/code/book_rzn/backend/config/cms.json"
+var path_config = "/home/yu/Desktop/code/book_rzn/backend/config/config.json"
 
 type Configuration struct {
-	Url_prefix    string `json:"url_prefix"`
-	Ip            string `json:"ip"`
-	Port          string `json:"port"`
-	Split_ip_port string `json:"split_ip_port"`
-	Path_prefix   string `json:"path_prefix"`
-	Path_frontend string `json:"path_frontend"`
-	Path_backend  string `json:"path_backend"`
-	Path_config   string `json:"path_config"`
-	Path_static   string `json:"path_static"`
-	Path_bd       string `json:"path_bd"`
-	Bd_admin_list string `json:"bd_admin_list"`
-	Bd_users_list string `json:"bd_users_list"`
-	Full_url_addr string
-	HostAndPort   string
-}
-
-type DataTemp struct {
+	Url_prefix         string   `json:"url_prefix"`
+	Ip                 string   `json:"ip"`
+	Port               string   `json:"port"`
+	Split_ip_port      string   `json:"split_ip_port"`
+	Path_prefix        string   `json:"path_prefix"`
+	Path_frontend      string   `json:"path_frontend"`
+	Path_backend       string   `json:"path_backend"`
+	Path_config        string   `json:"path_config"`
+	Path_static        string   `json:"path_static"`
+	Path_bd            string   `json:"path_bd"`
+	Bd_admin_list      string   `json:"bd_admin_list"`
+	Bd_users_list      string   `json:"bd_users_list"`
 	Phone              string   `json:"phone"`
 	Email              string   `json:"email"`
 	Footer_years       string   `json:"footer_years"`
@@ -38,7 +30,6 @@ type DataTemp struct {
 	Location_shops     []string `json:"location_shops"`
 	Path_logo_company  string   `json:"path_logo_company"`
 	Path_banner_home   string   `json:"path_banner_home"`
-	Full_url_addr      string   `json:"full_url_addr"`
 	Company_name       string   `json:"compny_name"`
 	Text_banner        string   `json:"text_banner"`
 	Description_banner string   `json:"description_banner"`
@@ -48,33 +39,9 @@ type DataTemp struct {
 	Cards_stronikum []string
 	Cards_agat      []string
 
-	Prosv_cards []parsing.ProsvCard
-}
-
-type Cms struct {
-	Url_prefix        string   `json:"url_prefix"`
-	Ip                string   `json:"ip"`
-	Port              string   `json:"port"`
-	Split_ip_port     string   `json:"split_ip_port"`
-	Path_prefix       string   `json:"path_prefix"`
-	Path_frontend     string   `json:"path_frontend"`
-	Path_backend      string   `json:"path_backend"`
-	Path_config       string   `json:"path_config"`
-	Path_static       string   `json:"path_static"`
-	Path_bd           string   `json:"path_bd"`
-	Bd_admin_list     string   `json:"bd_admin_list"`
-	Bd_users_list     string   `json:"bd_users_list"`
-	Phone             string   `json:"phone"`
-	Email             string   `json:"email"`
-	Footer_years      string   `json:"footer_years"`
-	Copyright         string   `json:"copyright"`
-	Location_office   string   `json:"location_office"`
-	Location_shops    []string `json:"location_shops"`
-	Path_logo_company string   `json:"path_logo_company"`
-	Path_banner_home  string   `json:"path_banner_home"`
-	Company_name      string   `json:"compny_name"`
-	Full_url_addr     string
-	HostAndPort       string
+	Prosv_cards   []parsing.ProsvCard
+	Full_url_addr string
+	HostAndPort   string
 }
 
 func NewConfiguration() Configuration {
@@ -83,36 +50,6 @@ func NewConfiguration() Configuration {
 	byteValue, _ := ioutil.ReadAll(file)
 	var conf Configuration
 	json.Unmarshal(byteValue, &conf)
-	conf.Full_url_addr = conf.Url_prefix + conf.Ip + conf.Split_ip_port + conf.Port
-	conf.HostAndPort = conf.Ip + conf.Split_ip_port + conf.Port
+	conf.Prosv_cards = parsing.GetLinks()
 	return conf
-}
-
-func NewDataTemp() DataTemp {
-	file, _ := os.Open(path_datatemp)
-	defer file.Close()
-	byteValue, _ := ioutil.ReadAll(file)
-	var dt DataTemp
-	json.Unmarshal(byteValue, &dt)
-	// dt.Prosv_cards = []ProsvCard{
-	// 	ProsvCard{"name", "desc", "100.90 r"},
-	// 	ProsvCard{"name", "desc", "100.90 r"},
-	// 	ProsvCard{"name", "desc", "100.90 r"},
-	// 	ProsvCard{"name", "desc", "100.90 r"},
-	// }
-
-	dt.Prosv_cards = parsing.GetLinks()
-	return dt
-}
-
-func NewCms() Cms {
-	file, _ := os.Open(path_datatempCMS)
-	defer file.Close()
-	byteValue, _ := ioutil.ReadAll(file)
-	var dtCMS Cms
-	json.Unmarshal(byteValue, &dtCMS)
-	dtCMS.Full_url_addr = dtCMS.Url_prefix + dtCMS.Ip + dtCMS.Split_ip_port + dtCMS.Port
-	dtCMS.HostAndPort = dtCMS.Ip + dtCMS.Split_ip_port + dtCMS.Port
-	fmt.Printf("%#v\n", dtCMS)
-	return dtCMS
 }
--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -8,69 +8,67 @@ import (
 	"text/template"
 )
 
-var DataTemp = config.NewDataTemp()
-var DataTempCMS = config.NewCms()
 var Config = config.NewConfiguration()
 
 func OpenHtmlAbout(w http.ResponseWriter, r *http.Request) {
 	tmpl, _ := template.ParseFiles(Config.Path_prefix + Config.Path_frontend + "about.html")
-	tmpl.Execute(w, DataTemp)
+	tmpl.Execute(w, Config)
 }
 
 func OpenHtmlBlog(w http.ResponseWriter, r *http.Request) {
 	tmpl, _ := template.ParseFiles(Config.Path_prefix + Config.Path_frontend + "blog.html")
-	tmpl.Execute(w, DataTemp)
+	tmpl.Execute(w, Config)
 }
 
 func OpenHtmlCart(w http.ResponseWriter, r *http.Request) {
 	tmpl, _ := template.ParseFiles(Config.Path_prefix + Config.Path_frontend + "cart.html")
-	tmpl.Execute(w, DataTemp)
+	tmpl.Execute(w, Config)
 }
 
 func OpenHtmlContacts(w http.ResponseWriter, r *http.Request) {
 	tmpl, _ := template.ParseFiles(Config.Path_prefix + Config.Path_frontend + "contacts.html")
-	tmpl.Execute(w, DataTemp)
+	tmpl.Execute(w, Config)
 }
 
 func OpenHtmlDelivery(w http.ResponseWriter, r *http.Request) {
 	tmpl, _ := template.ParseFiles(Config.Path_prefix + Config.Path_frontend + "delivery.html")
-	tmpl.Execute(w, DataTemp)
+	tmpl.Execute(w, Config)
 }
 
 func OpenHtmlExchange(w http.ResponseWriter, r *http.Request) {
 	tmpl, _ := template.ParseFiles(Config.Path_prefix + Config.Path_frontend + "exchange.html")
-	tmpl.Execute(w, DataTemp)
+	tmpl.Execute(w, Config)
 }
 func OpenHtmlFavorites(w http.ResponseWriter, r *http.Request) {
 	tmpl, _ := template.ParseFiles(Config.Path_prefix + Config.Path_frontend + "cart.html")
-	tmpl.Execute(w, DataTemp)
+	tmpl.Execute(w, Config)
 }
 func OpenHtmlHome(w http.ResponseWriter, r *http.Request) {
 	// fmt.Println("open home", Config.Path_prefix + Config.Path_frontend)
 	tmpl, _ := template.ParseFiles(Config.Path_prefix + Config.Path_frontend + "home.html")
-	tmpl.Execute(w, DataTemp)
+	tmpl.Execute(w, Config)
 }
 func OpenHtmlNew(w http.ResponseWriter, r *http.Request) {
 	tmpl, _ := template.ParseFiles(Config.Path_prefix + Config.Path_frontend + "new.html")
-	tmpl.Execute(w, DataTemp)
+	tmpl.Execute(w, Config)
 }
 
 func OpenHtmlPayment(w http.ResponseWriter, r *http.Request) {
 	tmpl, _ := template.ParseFiles(Config.Path_prefix + Config.Path_frontend + "payment.html")
-	tmpl.Execute(w, DataTemp)
+	tmpl.Execute(w, Config)
 }
 func OpenHtmlCollectschool(w http.ResponseWriter, r *http.Request) {
 	tmpl, _ := template.ParseFiles(Config.Path_prefix + Config.Path_frontend + "collect-school.html")
-	tmpl.Execute(w, DataTemp)
+	tmpl.Execute(w, Config)
 }
 func OpenHtmlLogin(w http.ResponseWriter, r *http.Request) {
 	tmpl, _ := template.ParseFiles(Config.Path_prefix + Config.Path_frontend + "login.html")
-	tmpl.Execute(w, DataTemp)
+	tmpl.Execute(w, Config)
 }
 
 func OpenHtmlRegistry(w http.ResponseWriter, r *http.Request) {
 	tmpl, _ := template.ParseFiles(Config.Path_prefix + Config.Path_frontend + "registration.html")
-	tmpl.Execute(w, DataTemp)
+	tmpl.Execute(w, Config)
 }
 
 func OpenHtmlCms(w http.ResponseWriter, r *http.Request) {
@@ -78,17 +76,17 @@ func OpenHtmlCms(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(res, "res cookie admin -- 404 ??")
 	if res {
 		tmpl, _ := template.ParseFiles(Config.Path_prefix + Config.Path_frontend + "cms.html")
-		tmpl.Execute(w, DataTempCMS)
+		tmpl.Execute(w, Config)
 	} else {
 		tmpl, _ := template.ParseFiles(Config.Path_prefix + Config.Path_frontend + "404.html")
-		tmpl.Execute(w, DataTempCMS)
+		tmpl.Execute(w, Config)
 	}
 
 }
 
 func OpenHtmlProfile(w http.ResponseWriter, r *http.Request) {
 	tmpl, _ := template.ParseFiles(Config.Path_prefix + Config.Path_frontend + "home.html")
-	tmpl.Execute(w, DataTemp)
+	tmpl.Execute(w, Config)
 }
 
 func OpenHtmlLoginCheck(w http.ResponseWriter, r *http.Request) {
@@ -124,38 +122,38 @@ func OpenHtmlLoginCheck(w http.ResponseWriter, r *http.Request) {
 
 func OpenHtmlBuyOrder(w http.ResponseWriter, r *http.Request) {
 	tmpl, _ := template.ParseFiles(Config.Path_prefix + Config.Path_frontend + "buy_order.html")
-	tmpl.Execute(w, DataTemp)
+	tmpl.Execute(w, Config)
 }
 
 func OpenHtml404(w http.ResponseWriter, r *http.Request) {
 	tmpl, _ := template.ParseFiles(Config.Path_prefix + Config.Path_frontend + "404.html")
-	tmpl.Execute(w, DataTemp)
+	tmpl.Execute(w, Config)
 }
 
 func OpenHtmlCreateUser(w http.ResponseWriter, r *http.Request) {
 	tmpl, _ := template.ParseFiles(Config.Path_prefix + Config.Path_frontend + "login.html")
-	tmpl.Execute(w, DataTemp)
+	tmpl.Execute(w, Config)
 }
 
 func OpenHtmlSales(w http.ResponseWriter, r *http.Request) {
 	tmpl, _ := template.ParseFiles(Config.Path_prefix + Config.Path_frontend + "sales.html")
-	tmpl.Execute(w, DataTemp)
+	tmpl.Execute(w, Config)
 }
 
 func OpenHtmlProsv(w http.ResponseWriter, r *http.Request) {
 	
 	tmpl, _ := template.ParseFiles(Config.Path_prefix + Config.Path_frontend + "prosv.html")
-	tmpl.Execute(w, DataTemp)
+	tmpl.Execute(w, Config)
 }
 func OpenHtmlAgat(w http.ResponseWriter, r *http.Request) {
 	tmpl, _ := template.ParseFiles(Config.Path_prefix + Config.Path_frontend + "agat.html")
-	tmpl.Execute(w, DataTemp)
+	tmpl.Execute(w, Config)
 }
 func OpenHtmlStronikum(w http.ResponseWriter, r *http.Request) {
 	tmpl, _ := template.ParseFiles(Config.Path_prefix + Config.Path_frontend + "stronikum.html")
-	tmpl.Execute(w, DataTemp)
+	tmpl.Execute(w, Config)
 }
 func OpenHtmlNaura(w http.ResponseWriter, r *http.Request) {
 	tmpl, _ := template.ParseFiles(Config.Path_prefix + Config.Path_frontend + "naura.html")
-	tmpl.Execute(w, DataTemp)
+	tmpl.Execute(w, Config)
 }
