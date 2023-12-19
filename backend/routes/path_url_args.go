0649632c853179fbe980e5ed586cebb8677c8586
YuranIgnatenko <Yuran.ignatenko@yandex.ru>
2023-12-19 11:07:32 +0300

    добавлен парсинг nau-ra

---
+++ backend/routes/path_url_args.go
@@ -0,0 +1,66 @@
+package routes
+
+import (
+	"strings"
+)
+
+type PathUrlArgs struct {
+	ArgRow  string
+	ArgCase string
+	Arg1    string
+	Arg2    string
+	Arg3    string
+}
+
+// разобрать url и получить  из него структуру с полями
+func NewPathUrlArgs(rUrlPath string) *PathUrlArgs {
+	var argRow, argCase, arg1, arg2, arg3 = "", "", "", "", ""
+
+	argRow = rUrlPath
+	argRow = strings.ReplaceAll(argRow, ":/", "://")
+	argRow = strings.ReplaceAll(argRow, `"`, "")
+	elems := strings.Split(argRow, "/")
+	elems = elems[1:]
+	count_elems := len(elems)
+
+	switch count_elems {
+	case 1:
+		argCase = elems[0]
+	case 2:
+		argCase = elems[0]
+		arg1 = elems[1]
+	case 3:
+		argCase = elems[0]
+		arg1 = elems[1]
+		arg2 = elems[2]
+	case 4:
+		argCase = elems[0]
+		arg1 = elems[1]
+		arg2 = elems[2]
+		arg3 = elems[3]
+		// default:
+	}
+	return &PathUrlArgs{
+		ArgRow:  argRow,
+		ArgCase: argCase,
+		Arg1:    arg1,
+		Arg2:    arg2,
+		Arg3:    arg3,
+	}
+}
+
+// получение фильтра из url (ex.: test.com?key1=value1)
+func (pua *PathUrlArgs) NewFilterUrlArgs(rUrlPath string) *PathUrlArgs {
+	var argRow, argCase = "", ""
+	argRow = rUrlPath
+	argRow = strings.ReplaceAll(argRow, ":/", "://")
+	argRow = strings.ReplaceAll(argRow, `"`, "")
+	elems := strings.Split(argRow, "/")
+	args := elems[len(elems)-1]
+	type_filter := strings.Split(strings.Split(args, "?")[0], "=")
+	return &PathUrlArgs{
+		ArgRow:  argRow,
+		ArgCase: argCase,
+		Arg1:    type_filter[1],
+	}
+}
--- backend/routes/tools.go
+++ backend/routes/tools.go
@@ -13,49 +13,6 @@ import (
 // 		rout.ConfirmOFFTableOrders(rout.GetCookieTokenValue(w, r), target_id_order)
 // 		http.Redirect(w, r, "/orders_history", http.StatusPermanentRedirect)
 
-type PathUrlArgs struct {
-	ArgRow  string
-	ArgCase string
-	Arg1    string
-	Arg2    string
-	Arg3    string
-}
-
-func NewPathUrlArgs(rUrlPath string) *PathUrlArgs {
-	var argRow, argCase, arg1, arg2, arg3 = "", "", "", "", ""
-
-	argRow = rUrlPath
-	argRow = strings.ReplaceAll(argRow, ":/", "://")
-	argRow = strings.ReplaceAll(argRow, `"`, "")
-	elems := strings.Split(argRow, "/")
-	elems = elems[1:]
-	count_elems := len(elems)
-
-	switch count_elems {
-	case 1:
-		argCase = elems[0]
-	case 2:
-		argCase = elems[0]
-		arg1 = elems[1]
-	case 3:
-		argCase = elems[0]
-		arg1 = elems[1]
-		arg2 = elems[2]
-	case 4:
-		argCase = elems[0]
-		arg1 = elems[1]
-		arg2 = elems[2]
-		arg3 = elems[3]
-		// default:
-	}
-	return &PathUrlArgs{
-		ArgRow:  argRow,
-		ArgCase: argCase,
-		Arg1:    arg1,
-		Arg2:    arg2,
-		Arg3:    arg3,
-	}
-}
 
 func GetFilterUrlArgs(rUrlPath string) *PathUrlArgs {
 	var argRow, argCase = "", ""
